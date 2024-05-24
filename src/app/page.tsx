da3c96ac9802298c0088c3e949aeb7e8b7b70fe0
danielshala <danielshala94@gmail.com>
2024-05-24 10:20:58 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import PokemonSearch from '../components/PokemonSearch';
 export default function Home() {
   return (
     <main>
-      <h1 style={{ textAlign: 'center', marginTop: '2rem' }}>Ricerca Pokémon</h1>
+      <h1 style={{ textAlign: 'center', marginTop: '2rem' }}>Search Pokémon</h1>
       <PokemonSearch />
     </main>
   );
